fe914e3a9455053a3f2a3f935fbfcfd607d41667
Florian <36770692+floriankrone@users.noreply.github.com>
2018-03-15 10:19:40 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+#Golang Server
+
+package main
+
+import (
+        "io"
+        "net/http"
+)
+
+func text(w http.ResponseWriter, r *http.Request) {
+        io.WriteString(w, "Es funktioniert!")
+}
+
+func main() {
+        http.HandleFunc("/", text)
+        http.ListenAndServe(":8000", nil)
+}
